6f7e5b05709e6167ad3ec38814974b4c4d54295d
unknown <rohangautam.it19@gmail.com>
2025-05-29 22:02:21 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react' 
+import './App.css'
+import AnimatedBox from './components/AnimatedBox'
+
+function App() {
+ 
+  return (
+    <>
+        <AnimatedBox />
+    </>
+  )
+}
+
+export default App
---
+++ src/utils/collection.js
@@ -0,0 +1,24 @@
+const collection = [
+    {title:"Profile 1", img: "/img1.jpg"},
+    {title:"Profile 2", img: "/img2.jpg"},
+    {title:"Profile 3", img: "/img3.jpg"},
+    {title:"Profile 4", img: "/img4.jpg"},
+    {title:"Profile 5", img: "/img5.jpg"},
+    {title:"Profile 6", img: "/img6.jpg"},
+    {title:"Profile 7", img: "/img7.jpg"},
+    {title:"Profile 8", img: "/img8.jpg"},
+    {title:"Profile 9", img: "/img9.jpg"},
+    {title:"Profile 10", img: "/img10.jpg"},
+    {title:"Profile 1", img: "/img1.jpg"},
+    {title:"Profile 2", img: "/img2.jpg"},
+    {title:"Profile 3", img: "/img3.jpg"},
+    {title:"Profile 4", img: "/img4.jpg"},
+    {title:"Profile 5", img: "/img5.jpg"},
+    {title:"Profile 6", img: "/img6.jpg"},
+    {title:"Profile 7", img: "/img7.jpg"},
+    {title:"Profile 8", img: "/img8.jpg"},
+    {title:"Profile 9", img: "/img9.jpg"},
+    {title:"Profile 10", img: "/img10.jpg"},
+];
+
+export default collection
